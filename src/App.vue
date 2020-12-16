a4590c7cb3535721be8d0d72840c12a7e4d2d843
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-16 16:52:14 +0000

    First component and props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <ul v-for="storedResource in storedResources" :key="storedResource.id">
-    <li>Vue.js</li>
-    <li>Google</li>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
   </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource,
+  },
+
   data() {
     return {
       storedResources: [
         {
           id: 'official-guide',
           title: 'Official Guide',
-          description: 'The official Vue.js documentation',
+          description: 'The official Vue.js documentation.',
           link: 'https://vuejs.org'
         },
 
