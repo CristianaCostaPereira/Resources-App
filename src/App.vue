e24adf9d185883dc1617d6cad3e702556f3725de
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-17 18:22:59 +0000

    Tabs added and style; provide-inject

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
   <the-header title="Documentation"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    TheHeader
+    TheHeader,
+    TheResources
   },
-
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org'
-        },
-
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.pt'
-        },
-      ],
-    };
-  }
 }
 </script>
 
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,7 +1,7 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-    <base-button @click="setSelectedTab('add-resource ')">Add Resources</base-button>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButton">Add Resources</base-button>
   </base-card>
 
   <component :is="selectedTab"></component>
@@ -19,8 +19,40 @@ export default {
 
   data() {
     return {
-      selectedTab: 'stored-resources'
+      selectedTab: 'stored-resources',
+
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.pt'
+        },
+      ],
     };
+  },
+
+  provide() {
+    return{
+      resources: this.storedResources
+    };
+  },
+
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+
+    addResButton() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    }
   },
 
   methods: {
