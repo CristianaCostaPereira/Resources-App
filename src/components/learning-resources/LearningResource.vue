a3b09996bb4a2b3b4f991d77e524d99dd9f635d6
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-17 11:29:36 +0000

    Slots and basic card component

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,17 +1,17 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
 
-    <p>{{ description }}</p>
+      <p>{{ description }}</p>
 
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
